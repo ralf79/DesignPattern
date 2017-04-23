34abe0c7260a61454ffaafad1b7742a9cc848062
ralf1979 <ralf79@kiai.kr>
2017-04-24 01:15:07 +0900

    Composite Commit

---
+++ src/org/kosta/Unit.java
@@ -0,0 +1,10 @@
+package org.kosta;
+
+public interface Unit {
+	 public void attack();
+	 public void move();
+	 public void add(Unit unit);
+	 public void remove(Unit unit);
+	 public Unit get(int i);
+	 
+	}
--- src/org/kosta/main.java
+++ src/org/kosta/main.java
@@ -1,26 +1,24 @@
 package org.kosta;
 
 public class Main {
-    public static void main(String[] args) {
-        if(args.length!=1){
-            System.exit(0);
-        }
-        if(args[0].equals("plain")){
-            TextBuilder textBuilder=new TextBuilder();
-            Director director=new Director(textBuilder);
-            director.construct();
-            String result=textBuilder.getResult();
-            System.out.println(result);
-        }
-        else if(args[0].equals("html")){
-            HTMLBuilder htmlBuilder=new HTMLBuilder();
-            Director director=new Director(htmlBuilder);
-            director.construct();
-            String fileName=htmlBuilder.getResult();
-            System.out.println(fileName+"가 작성되었습니다!");
-        }
-        else{
-            System.exit(0);
-        }
-    }
+
+	public static void main(String[] args) {
+		Unit leaf1 = new Terran("Marin");
+		  Unit leaf2 = new Terran("Firebet");
+		  Unit leaf3 = new Terran("SiegeTank");
+		  
+		  Platoon composite = new Platoon("1소대");
+		  
+		  composite.add(leaf1);
+		  composite.add(leaf2);
+		  composite.add(leaf3);
+		  
+		  composite.attack();
+		  composite.move();
+		  
+		  leaf1.move();
+
+
+	}
+
 }
