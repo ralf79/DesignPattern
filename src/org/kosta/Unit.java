281d02890a5ae6ea3a71d6be6bac203d0806a028
ralf1979 <ralf79@kiai.kr>
2017-04-24 04:46:02 +0900

    Composite init

--- src/org/kosta/Unit.java
+++ src/org/kosta/Unit.java
@@ -1,10 +1,4 @@
 package org.kosta;
 
 public interface Unit {
-	 public void attack();
-	 public void move();
-	 public void add(Unit unit);
-	 public void remove(Unit unit);
-	 public Unit get(int i);
-	 
-	}
+}
