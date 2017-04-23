4fb44d95178831553e13143b49243cc39a73e527
ralf1979 <ralf79@kiai.kr>
2017-04-24 03:17:59 +0900

    FM init

--- src/framework/Main.java
+++ src/framework/Main.java
@@ -6,17 +6,6 @@ import framework.idcard.IDCardFactory;
 public class Main {
 
 	public static void main(String[] args) {
-		Factory factory = new IDCardFactory();
-        Product card1 = factory.create("È«±æµ¿");
-        Product card2 = factory.create("ÆÛÁñÀÙ");
-        Product card3 = factory.create("ÀÚ¹Ù");
- 
-        card1.use();
-        card2.use();
-        card3.use();
-
-        System.out.println(((IDCardFactory)factory).getOwners());
-
 	}
 
 }
--- src/framework/Product.java
+++ src/framework/Product.java
@@ -1,6 +1,5 @@
 package framework;
 
 public abstract class Product {
-    public abstract void use();
 }
 
