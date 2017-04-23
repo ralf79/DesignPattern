cf08fced3a6005d92cb85ac5a9e1547828edb508
ralf1979 <ralf79@kiai.kr>
2017-04-24 03:57:19 +0900

    prototype init

--- src/framework/Main.java
+++ src/framework/Main.java
@@ -3,20 +3,6 @@ package framework;
 public class Main {
 
 	public static void main(String[] args) {
-		 Manager manager=new Manager();
-	        UnderlinePen upen=new UnderlinePen('~');
-	        MessageBox mbox=new MessageBox('*');
-	        MessageBox sbox=new MessageBox('/');
-	        manager.register("strong", upen);
-	        manager.register("warning", mbox);
-	        manager.register("slash", sbox);
-
-	        Product p1=manager.create("strong");
-	        p1.use("Hello,world.");
-	        Product p2=manager.create("warning");
-	        p2.use("Hello,world.");
-	        Product p3=manager.create("slash");
-	        p3.use("Hello,world.");
 
 
 	}
--- src/framework/Manager.java
+++ src/framework/Manager.java
@@ -3,14 +3,6 @@ package framework;
 import java.util.HashMap;
 
 public class Manager {
-    private HashMap<String, Product> showcase=new HashMap<String, Product>();
-    public void register(String name, Product proto){
-        showcase.put(name, proto);
-    }
-    public Product create(String protoname){
-        Product p= showcase.get(protoname);
-        return p.createClone();
-    }
 }
 
 
