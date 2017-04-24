60ca5698bc237dcd3cc7c17faae958c533f5325f
ralf1979 <ralf79@kiai.kr>
2017-04-25 06:47:01 +0900

    gg

--- src/org/kosta/Airplane.java
+++ src/org/kosta/Airplane.java
@@ -4,26 +4,4 @@ public class Airplane extends Thread {
 	private final ControlTower tower;
 	private final int seq;
 
-	public Airplane(ControlTower tower, int seq) {
-		this.tower = tower;
-		this.seq = seq;
-	}
-
-	public int getSeq() {
-		return seq;
-	}
-
-	@Override
-	public void run() {
-		try {
-			
-			while (!tower.getPermission()) {
-				System.out.println(seq +"번 째 비행기 대기 중.");
-				Thread.sleep(500);
-			}
-			tower.land(this);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
 }
--- src/org/kosta/Main.java
+++ src/org/kosta/Main.java
@@ -3,14 +3,7 @@ package org.kosta;
 public class Main {
 
 	public static void main(String[] args) {
-		ControlTower tower = new ControlTower();
-        Airplane[] airplanes = new Airplane[10];
-        for (int i = 0; i < airplanes.length; i++) {
-            airplanes[i] = new Airplane(tower, i);
-        }
-        for (Airplane airplane : airplanes) {
-            airplane.start();
-        }
+
 	}
 
 }
