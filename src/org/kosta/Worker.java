326977ee052a2addd1fb8b9508a60639c56e4e5f
ralf1979 <ralf79@kiai.kr>
2017-04-25 06:03:13 +0900

    gg

--- src/org/kosta/Worker.java
+++ src/org/kosta/Worker.java
@@ -3,7 +3,7 @@ package org.kosta;
 public abstract class Worker {
 	protected abstract void doit();
 
-	public final void work() {
+	public void work() {
 		System.out.println("Ãâ±Ù");
 		doit();
 		System.out.println("Åð±Ù");
