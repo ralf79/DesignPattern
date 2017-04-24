59d3d470b88e41844d1f966d8109e00f95886575
ralf1979 <ralf79@kiai.kr>
2017-04-24 19:57:33 +0900

    gg

--- src/org/kosta/Main.java
+++ src/org/kosta/Main.java
@@ -2,43 +2,16 @@ package org.kosta;
 
 public class Main {
 
-	
-	private static CoffeeFlavor[] flavors = new CoffeeFlavor[100];
-    private static CoffeeOrderContext[] tables = new CoffeeOrderContext[100];
-    private static int ordersMade = 0;
-    private static CoffeeFlavorFactory flavorFactory;
-   
-    public static void takeOrders(String flavorIn, int table) {
-        flavors[ordersMade] = flavorFactory.getCoffeeFlavor(flavorIn);
-        tables[ordersMade++] = new CoffeeOrderContext(table);
+    public static void main(String[] args) {
+
+        Printable p = new PrinterProxy("Alice");
+        System.out.println("이름은 현재 " + p.getPrinterName() + "입니다.");
+        
+        p.setPrinterName("Bob");
+        System.out.println("이름은 현재 " + p.getPrinterName() + "입니다.");
+        
+        p.print("Hello, world.");
+
     }
-	public static void main(String[] args) {
-
-		flavorFactory = new CoffeeFlavorFactory();
-	      
-        takeOrders("Cappuccino", 2);
-        takeOrders("Cappuccino", 2);
-        takeOrders("Frappe", 1);
-        takeOrders("Frappe", 1);
-        takeOrders("Xpresso", 1);
-        takeOrders("Frappe", 897);
-        takeOrders("Cappuccino", 97);
-        takeOrders("Cappuccino", 97);
-        takeOrders("Frappe", 3);
-        takeOrders("Xpresso", 3);
-        takeOrders("Cappuccino", 3);
-        takeOrders("Xpresso", 96);
-        takeOrders("Frappe", 552);
-        takeOrders("Cappuccino", 121);
-        takeOrders("Xpresso", 121);
-   
-        for (int i = 0; i < ordersMade; ++i) {
-            flavors[i].serveCoffee(tables[i]);
-        }
-        System.out.println(" ");
-        System.out.println("total CoffeeFlavor objects made: " +  flavorFactory.getTotalCoffeeFlavorsMade());
-
-	}
 
 }
-
---
+++ src/org/kosta/Printable.java
@@ -0,0 +1,11 @@
+package org.kosta;
+
+public interface Printable {
+
+    public abstract void setPrinterName(String name); 
+
+    public abstract String getPrinterName(); 
+
+    public abstract void print(String string); 
+
+}
