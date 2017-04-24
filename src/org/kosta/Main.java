ed6ef2979ff51afb12da7b2ee3611c263cd6b023
ralf1979 <ralf79@kiai.kr>
2017-04-25 07:27:27 +0900

    gg

--- src/org/kosta/Main.java
+++ src/org/kosta/Main.java
@@ -3,15 +3,7 @@ package org.kosta;
 public class Main {
 
 	public static void main(String[] args) {
-		TV tv = new TV();
-        Audio audio = new Audio();
-        Light light = new Light();
-        
-        Home home = new Home(audio, light, tv);
-        
-        home.enjoyTv();
-        home.enjoyMusic();
-        home.goOut();
+		
 	}
 
 }
--- src/org/kosta/TV.java
+++ src/org/kosta/TV.java
@@ -1,19 +1,5 @@
 package org.kosta;
 
 public class TV {
-	private boolean turnedOn = false;
-
-	public void turnOn() {
-		turnedOn = true;
-		System.out.println("TV¸¦ ÄÔ.");
-	}
-
-	public void turnOff() {
-		turnedOn = false;
-		System.out.println("TV¸¦ ²û.");
-	}
-
-	public boolean isTurnedOn() {
-		return turnedOn;
-	}
+	
 }
