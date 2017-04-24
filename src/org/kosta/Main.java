5f4ff9ad222087a4c0cfa675045f7759a3494154
ralf1979 <ralf79@kiai.kr>
2017-04-25 06:50:31 +0900

    gg

--- src/org/kosta/Main.java
+++ src/org/kosta/Main.java
@@ -4,17 +4,6 @@ import java.util.Iterator;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Units units = new Units();
-		units.add("히드라");
-		units.add("마린");
-		units.add("질럿");
-
-		Iterator<String> iterator = units.iterator();
-		while (iterator.hasNext()) {
-			String element = iterator.next();
-			System.out.println(element);
-		}
-	}
+	
 
 }
--- src/org/kosta/Units.java
+++ src/org/kosta/Units.java
@@ -5,27 +5,5 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Units implements Iterable<String> {
-	private List<String> list = new ArrayList<String>();
-
-	public void add(String name) {
-		list.add(name);
-	}
-
-	public Iterator<String> iterator() {
-		return new Iterator<String>() {
-			int seq = 0;
-
-			public boolean hasNext() {
-				return seq < list.size();
-			}
-
-			public String next() {
-				return list.get(seq++);
-			}
-
-			public void remove() {
-				throw new UnsupportedOperationException();
-			}
-		};
-	}
+	
 }
