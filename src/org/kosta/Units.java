7678f1557035305deec2ea474005cd51207ff743
ralf1979 <ralf79@kiai.kr>
2017-04-25 11:21:19 +0900

    gg

--- src/org/kosta/Units.java
+++ src/org/kosta/Units.java
@@ -5,5 +5,11 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Units implements Iterable<String> {
+	private List<String> lists = new ArrayList<>();
 	
+	public void add(String name) {
+		lists.add(name);
+	}
+	
+	public Iterator<Stirng>iterator
 }
