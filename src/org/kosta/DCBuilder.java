5e71bae07b676e519b6e42d210b634d9c922e7b0
hyunwoo Lee <hw.ralf79@gmail.com>
2017-05-23 13:50:20 +0900

    change code

--- src/org/kosta/DCBuilder.java
+++ src/org/kosta/DCBuilder.java
@@ -1,22 +1,23 @@
 package org.kosta;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-
 public class DCBuilder implements Builder{
-	private Hero batman;
+
+	@Override
+	public void makeArm() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void makeLeg() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Hero getResult() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
-	DCBuilder(){
-        batman = new Hero("¹èÆ®¸Ç");
-    }
-    public void makeArm() {
-        batman.setArmSource("µ·Áö¶ö");
-    }
-    public void makeLeg() {
-        batman.setLegSource("µ·Áö¶ö");
-    }
-    public Hero getResult() {
-        return batman;
-    }
 }
--- src/org/kosta/Director.java
+++ src/org/kosta/Director.java
@@ -1,16 +1,6 @@
 package org.kosta;
 
 public class Director {
-	private Builder builder;
-    public Director(Builder builder) {
-        this.builder = builder;
-    }
-    public void build(){
-        builder.makeArm();
-        builder.makeLeg();
-    }
-    public Hero getHero(){
-        return builder.getResult();
-    }
+	
 }
 
