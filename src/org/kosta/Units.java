36ad09f8338d1ca487df728ccc75eaa625465178
hyunwoo Lee <hw.ralf79@gmail.com>
2017-05-25 14:16:31 +0900

    change code

--- src/org/kosta/Units.java
+++ src/org/kosta/Units.java
@@ -5,11 +5,4 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Units implements Iterable<String> {
-	private List<String> lists = new ArrayList<>();
-	
-	public void add(String name) {
-		lists.add(name);
-	}
-	
-	public Iterator<Stirng>iterator
 }
