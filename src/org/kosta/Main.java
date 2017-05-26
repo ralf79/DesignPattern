29258ac597a4c175f7ea5978a62d34494c99347d
hyunwoo Lee <hw.ralf79@gmail.com>
2017-05-26 18:34:19 +0900

    code c

--- src/org/kosta/Main.java
+++ src/org/kosta/Main.java
@@ -1,24 +1,11 @@
 package org.kosta;
 
-import java.util.ArrayList;
-
 import org.kosta.Button.OnClickListener;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Button button = new Button();
 		
-		
-		button.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(Button button) {
-				System.out.println(button+" is Clicked");
-			}
-		});
-		
-		button.onClick();
 	}
 
 }
